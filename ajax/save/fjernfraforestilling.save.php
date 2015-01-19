82c2c4674e793399db518a70401c914b068caaa3
Marius Mandal <mariusmandal@gmail.com>
2015-01-19 22:25:22 +0100

    Legg til hooks for UKMprogram_save
    Forbereder UKMcache-modulen

--- ajax/save/fjernfraforestilling.save.php
+++ ajax/save/fjernfraforestilling.save.php
@@ -1,9 +1,12 @@
 <?php
 function UKMdeltakere_save() {
+	
 	$innslag = new innslag($_POST['b_id']);	
 	if(get_option('site_type')!='kommune')
 		$innslag->videresendte(get_option('pl_id'));
 	$res = $innslag->fjernfraforestilling(str_replace('forestilling_','',$_POST['c_id']));
+	
+	do_action('UKMprogram_save', 'fjern_fra_forestilling', $_POST['c_id']);
 	die(json_encode(array('result'=>(bool)$res)));
 }
 ?>
--- ajax/save/leggtiliforestilling.save.php
+++ ajax/save/leggtiliforestilling.save.php
@@ -6,6 +6,8 @@ function UKMdeltakere_save() {
 	$concert->addBand($_POST['b_id']);
 	
 	$data = array('forestilling'=>$_POST['c_id'], 'innslag'=>$_POST['b_id']);
+	do_action('UKMprogram_save', 'leggtil_i_forestilling', $_POST['c_id'], $_POST['b_id']);
+
 	die(json_encode($data));
 }
 ?>
