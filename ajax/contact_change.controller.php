e0ca7f9259b48167b0b54b85c12cdfa3f9250cbf
AsgeirSH <askhus@online.no>
2016-10-13 16:39:12 +0200

    Endring av kontaktperson med advarsel

--- ajax/contact_change.controller.php
+++ ajax/contact_change.controller.php
@@ -9,8 +9,13 @@ foreach( $innslag->getPersoner()->getAll() as $person ) {
 	$JSON->personer[] = data_person( $person );
 }
 
+$JSON->kontaktperson = new stdClass();
+$JSON->kontaktperson->id = $innslag->getKontaktperson()->getId();
+$JSON->kontaktperson->fornavn = $innslag->getKontaktperson()->getFornavn();
+$JSON->kontaktperson->etternavn = $innslag->getKontaktperson()->getEtternavn();
+$JSON->kontaktperson->deltar = null;	
 if( !in_array( $innslag->getKontaktperson()->getId(), $ids ) ) {
 	$JSON->personer[] = data_person( $innslag->getKontaktperson() );
+	$JSON->kontaktperson->deltar = false;	
 }
 
-$JSON->kontaktperson = $innslag->getKontaktperson()->getId();
