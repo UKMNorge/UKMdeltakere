b63d665325c5886f101482d7b93c5a13800ba352
AsgeirSH <askhus@online.no>
2016-10-17 19:10:56 +0200

    Prøv å unngå en ny Selma-scare

--- save/innslag.save.php
+++ save/innslag.save.php
@@ -2,6 +2,11 @@
 
 require_once('UKM/write_innslag.class.php');
 
+// Sjekk for påkrevd data:
+if( empty($DATA['navn']) || empty($_POST['innslag']) || empty($DATA['kommune']) ) {
+	throw new Exception("Innslag.save: Mangler noe data!")
+}
+
 $innslag = new write_innslag( $_POST['innslag'] );
 
 $innslag->setNavn( $DATA['navn'] );
