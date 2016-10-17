25b8b12d7ea89ea2fb827efaf09ae5adb85b157a
AsgeirSH <askhus@online.no>
2016-10-17 16:19:05 +0200

    Rediger tittelløse innslag

--- ajax/save.controller.php
+++ ajax/save.controller.php
@@ -11,6 +11,7 @@ UKMlogger::setID( 'wordpress', $current_user->ID, get_option('pl_id') );
 $DATA = [];
 if(is_array($_POST['formData'])) {
 	foreach( $_POST['formData'] as $field ) {
+		// TODO: Denne håndterer ikke array som verdier (ie. funksjoner[] som navn).
 		$DATA[ $field['name'] ] = $field['value'];
 	}
 }
@@ -42,6 +43,8 @@ switch( $_POST['doSave'] ) {
 	    require_once( plugin_dir_path( __FILE__ ). '../save/contact.save.php' );
 	    break;
 	case 'tittellos':
+		require_once( plugin_dir_path( __FILE__ ). '../save/innslag_tittellos.save.php');
+		break;
 	default:
 		throw new Exception("NOT IMPLEMENTED!");
 		break;
