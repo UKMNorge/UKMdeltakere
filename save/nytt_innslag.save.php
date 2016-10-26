3340ee04384782eed67823f77a6f7106571a7f8a
AsgeirSH <askhus@online.no>
2016-10-26 12:58:49 +0200

    Lagre sjanger, beskrivelse og evt kontaktperson som deltaker

--- save/nytt_innslag.save.php
+++ save/nytt_innslag.save.php
@@ -10,14 +10,36 @@ if(null == $DATA['kontakt']) {
 } else {
 	$kontaktperson = new write_person($DATA['kontakt']);
 }
+
 $kommune = new kommune( $DATA['kommune'] );
 $monstring = new monstring_v2( get_option('pl_id') );
 $type = innslag_typer::getByName($DATA['type']);
 $navn = $DATA['navn'];
+$sjanger = $DATA['sjanger'];
+$beskrivelse = $DATA['beskrivelse'];
+
+$kontaktpersonSomDeltaker = false;
+if( isset($DATA['kontaktpersonErMed']) && 'on' == $DATA['kontaktpersonErMed']) {
+	$kontaktpersonSomDeltaker = true;
+}
 
 $id = write_innslag::create($kommune, $monstring, $type, $navn, $kontaktperson );
 if( !is_numeric($id) ) {
 	throw new Exception("Klarte ikke å opprette nytt innslag.");
 }
+
+
+$innslag = new write_innslag($id);
+$innslag->setSjanger($sjanger);
+$innslag->setBeskrivelse($beskrivelse);
+if( true == $kontaktpersonSomDeltaker ) {
+	if( !$innslag->getPersoner()->leggTil( $kontaktperson ) ) {
+		// Lagre innslaget før feilmelding, sånn at sjanger og beskrivelse ikke forsvinner.
+		$innslag->save();
+		throw new Exception("Klarte ikke å legge til kontaktpersonen i innslaget!");
+	}
+}
+$innslag->save();
+
 $JSON->innslag_id = $id;
 $JSON->type = $type;
