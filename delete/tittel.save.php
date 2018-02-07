638451413d33931f691a9daf22ca198f716ea479
Marius Mandal <mariusmandal@gmail.com>
2018-02-07 11:52:31 +0100

    Fiks sletting av titler

--- delete/tittel.save.php
+++ delete/tittel.save.php
@@ -6,9 +6,9 @@ require_once('UKM/write_tittel.class.php');
 require_once('UKM/titler.collection.php'); 
 
 $innslag = new write_innslag($_POST['innslag']);
-$monstring = new monstring_v2(get_option('pl_id'));
+$monstring = new write_monstring(get_option('pl_id'));
 
-$title_collection = new titler($innslag->getId(), $innslag->getType(), $monstring);
+$title_collection = $innslag->getTitler( $monstring );
 
 switch($innslag->getType()->getKey()) {
 	case 'musikk':
@@ -17,15 +17,15 @@ switch($innslag->getType()->getKey()) {
 	case 'teater':
 	case 'litteratur':
 		$tittel = new write_tittel( $_POST['object_id'], 'smartukm_titles_scene');
-		$title_collection->fjern($tittel);
+		$title_collection->fjern($tittel, $monstring);
 		break;
 	case 'video':
 		$tittel = new write_tittel( $_POST['object_id'], 'smartukm_titles_video');
-		$title_collection->fjern($tittel);
+		$title_collection->fjern($tittel, $monstring);
 		break;
 	case 'utstilling':
 		$tittel = new write_tittel( $_POST['object_id'], 'smartukm_titles_exhibition');
-		$title_collection->fjern($tittel);
+		$title_collection->fjern($tittel, $monstring);
 		break;
 	default:
 		throw new Exception("Tittel-save: Kan kun fjerne titler for scene, video eller utstilling, ikke ". $innslag->getType()->getKey());
