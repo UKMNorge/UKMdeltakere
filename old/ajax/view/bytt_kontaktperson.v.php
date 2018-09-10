4d10aa5b9b1901d664115414fdd3b9621b0e1bce
Marius Mandal <mariusmandal@gmail.com>
2018-09-10 22:08:00 +0200

    Slettet old-filer

--- old/ajax/view/bytt_kontaktperson.v.php
+++
@@ -1,9 +0,0 @@
-<?php function UKMdeltakere_ajax_view($c){ ?>
-
-<div class="help_text">
-	<p>
-		For å bytte kontaktperson, klikk på en av de grå hodene ved siden av et av navnene.
-	</p>
-</div>
-
-<?php } ?>
