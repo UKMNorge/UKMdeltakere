254aa4b6625dbac6d8459762b400eb72f478915f
Christian <christian.hvatum.ch@hotmail.com>
2020-01-10 10:14:44 +0100

    Fjernet kategori 3. Rettet opp søk-filter 🔍
    Fix #49

--- controller/layout.controller.php
+++ controller/layout.controller.php
@@ -4,20 +4,18 @@ use UKMNorge\Arrangement\Arrangement;
 
 require_once('UKM/Autoloader.php');
 
-if( !get_option('pl_id') ) {
+if ( !get_option('pl_id')) {
     UKMdeltakere::setAction('registrer_monstring');
 } else {
-    $monstring = new Arrangement( get_option('pl_id') );
+    $monstring = new Arrangement(get_option('pl_id'));
 
     $ufullstendige = $monstring->getInnslag()->getAllUfullstendige();
-    $pabegynte 		= $monstring->getInnslag()->filterByStatus( array(0,1,2,3,4), $ufullstendige );
 
     $stat = new stdClass();
-    $stat->fullstendige 	= $monstring->getInnslag()->getAntall();
-    $stat->ufullstendige 	= sizeof( $ufullstendige ) - sizeof( $pabegynte );
-    $stat->pabegynte 		= sizeof( $pabegynte );
+    $stat->fullstendige = $monstring->getInnslag()->getAntall();
+    $stat->ufullstendige = sizeof($ufullstendige);
 
-    $TWIGdata['tab_active'] = isset( $_GET['list'] ) ? $_GET['list'] : 'fullstendig';
+    $TWIGdata['tab_active'] = isset($_GET['list']) ? $_GET['list'] : 'fullstendig';
     $TWIGdata['monstring'] = $monstring;
     $TWIGdata['stat'] = $stat;
 }
--- controller/list_ufullstendig.controller.php
+++ controller/list_ufullstendig.controller.php
@@ -7,5 +7,6 @@ $collection = $monstring->getInnslag();
 $TWIGdata['list_innslag'] = [];
 
 foreach( $monstring->getInnslagTyper( true ) as $type_innslag ) {
-	$TWIGdata['list_innslag'][ $type_innslag->getKey() ] = $collection->filterByStatus([7,6,5], $collection->filterByType( $type_innslag, $collection->getAllUfullstendige() ) );
+    $TWIGdata['list_innslag'][ $type_innslag->getKey() ] = $collection->filterByType( $type_innslag, $collection->getAllUfullstendige());
+// $TWIGdata['list_innslag'][ $type_innslag->getKey() ] = $collection->filterByStatus([7,6,5], $collection->filterByType( $type_innslag, $collection->getAllUfullstendige() ) );
 }
